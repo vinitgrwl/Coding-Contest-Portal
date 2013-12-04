3c14c6a3a4f3ce632736da5c05ea495de66afb4f
vinitgrwl <vinitgrwl6@gmail.com>
2013-12-05 02:15:23 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,8 @@
+<!-- Made by Vinit Agarwal, Tanya Agarwal and Rashi Gupta -->
+<?php
+//hour,minute,second,month,day,year
+$start_time=mktime(16,0,0,15,4,2012);
+$end_time=mktime(21,0,0,3,2,2012);
+$STORE_PATH="/Applications/xampp/htdocs/dbms_project_final/checking";
+$p="/Applications/xampp/htdocs/dbms_project_final";
+?>
