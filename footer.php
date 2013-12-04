5507100955f5cd7e13e3c7a2b58634989eab9f20
vinitgrwl <vinitgrwl6@gmail.com>
2013-12-05 02:12:23 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!-- Made by Vinit Agarwal, Tanya Agarwal and Rashi Gupta -->
+    <!-- begin #footer -->
+    <div id="footer">
+   	  <p>
+        	Copyright &copy; 2012. Designed by <br />
+            <a class="footerLink"><abbr title="eXtensible HyperText Markup Language">Tanya</abbr></a>|
+      <a class="footerLink"><abbrev>Rashi</abbrev></a> | <a class="footerLink"><abbrev>Vinit</abbrev></a></p>
+        <div style="text-align:center;"></div> 
+    </div>
+    <!-- end #footer -->
