2c8da0638528bcc6f62ba8f997a9c561dd565874
vinitgrwl <vinitgrwl6@gmail.com>
2013-12-05 02:13:49 +0530

    Create home1.php

---
+++ home1.php
@@ -0,0 +1,85 @@
+<!-- Made by Vinit Agarwal, Tanya Agarwal and Rashi Gupta -->
+<?php
+session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>LNMIIT</title>
+<link href="style1.css" rel="stylesheet" type="text/css" /><!--[if IE 5]-->
+</head>
+<body>
+<!-- begin #container -->
+<div id="container">
+	<!-- begin #header -->
+    <div id="header">
+	<div id="hpic"><input type="image" src="images/LNMIIT-logo.jpg" alt="" width="150" height="82" align="left" align="top" margin-left=5px" /></div>
+	<h1>LNMIIT CODING CONTEST</h1>
+		<div class="headerTop"> 
+</div>
+       <div class="mainMenu">
+	   <a href="home.php">Problems</a>&nbsp &nbsp
+	  <a href="submit.php">Submit</a>&nbsp &nbsp
+	  <a href="statistics.php">Status</a>&nbsp &nbsp 
+	  <a href="score.php">Score</a>&nbsp &nbsp 
+	  <a href="contact_us.php">Contact us</a>&nbsp &nbsp
+	   </div>
+	  
+        <div class="headerPic">
+		<a>Time</a>
+	   <body onLoad="show_clock()" >
+	   <script language="javascript" src="liveclock.js">
+</script>
+        </div>
+		
+    </div>
+    <!-- end #header -->
+	<div id="main content">
+
+
+<html>
+<title>home</title>
+<body>
+<?php
+$host="localhost";
+$username="root";
+$password="";
+$db_name="codechef";
+$tbl_name="problems";
+$con=mysql_connect("$host","$username","$password")or die("cannot connect");
+mysql_select_db("$db_name")or die("cannot select DB");
+$uid=$_SESSION['uid'];
+$date=date("Y/m/d");
+echo $uid;
+$sql="SELECT * FROM $tbl_name where datetime='$date'";
+$result=mysql_query($sql);
+$count=mysql_num_rows($result);
+$i=1;
+while($info=mysql_fetch_array($result))
+{
+$title=$info['title'];
+$code=$info['qcode'];
+echo $i.".  ";
+echo "<a href='prob1.php?qcode=$code'>$title</a>";
+echo "    ";
+echo " Points:".$info['credits'];
+echo "<br/>";
+$i=$i+1;
+}
+mysql_close($con);
+?>
+</div>
+<br class="clearfloat" />
+    <!-- begin #footer -->
+    <div id="footer">
+   	  <p>
+        	Copyright &copy; 2012. Designed by <br />
+            <a class="footerLink"><abbr title="eXtensible HyperText Markup Language">Tanya</abbr></a>|
+      <a class="footerLink"><abbrev>Rashi</abbrev></a> | <a class="footerLink"><abbrev>Vinit</abbrev></a></p>
+        <div style="text-align:center;"></div> 
+    </div>
+    <!-- end #footer -->
+</div>
+<!-- end #container -->
+
